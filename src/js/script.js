c4ec7772dbc6d5605fcdebf82e4c2ced57950ed0
Alex Bui <zanbowie138@gmail.com>
2023-06-10 19:10:20 -0500

    hopefully working github page

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,12 @@
 import * as THREE from 'three';
 
+const canvas = document.querySelector('canvas.webgl')
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({canvas: canvas});
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
