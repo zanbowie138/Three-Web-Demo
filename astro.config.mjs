a62e2e25851bc5239af92781ee1f4dc3ef0494b6
Alex Bui <zanbowie138@gmail.com>
2023-06-16 17:22:27 -0500

    idk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://zanbowie138.github.io',
+    // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    sitemap: true,
+    base: './Three-Web-Demo/',
+});
